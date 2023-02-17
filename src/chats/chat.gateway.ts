6f7bc17775f40842586a94c72f1938fa3ac3f56e
Divine572 <divinef837@gmail.com>
2023-02-17 10:03:23 -0800

    added the creating of message and getting al messages in chat gateway class

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -8,6 +8,7 @@ import {
     ConnectedSocket
   } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { MessageDto } from './dto/message.dto';
    
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayConnection {
@@ -21,12 +22,26 @@ export class ChatGateway implements OnGatewayConnection {
     }
    
     @SubscribeMessage('send_message')
-    async listenForMessages(@MessageBody() message: string, @ConnectedSocket() socket: Socket) {
+    async listenForMessages(@MessageBody() message: MessageDto, @ConnectedSocket() socket: Socket) {
 
         const user = await this.chatsService.getUserFromSocket(socket)
-        this.server.sockets.emit('receive_message', {
-            message,
-            user
-        });
+        const newmessage = await this.chatsService.createMessage(message, user._id)
+
+        this.server.sockets.emit('receive_message', newmessage);
+
+        return newmessage
     }
+
+    @SubscribeMessage('get_all_messages')
+    async getAllMessages(@ConnectedSocket() socket: Socket) {
+
+        await this.chatsService.getUserFromSocket(socket)
+        const messages = await this.chatsService.getAllMessages()
+
+        this.server.sockets.emit('receive_message', messages);
+
+        return messages
+    }
+
+
 }
