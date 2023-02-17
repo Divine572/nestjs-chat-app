2d712b68346b71ddb64e7a19abf3d3d920a2989f
Divine572 <divinef837@gmail.com>
2023-02-17 06:53:22 -0800

    added mongo uri to .env

--- src/shared/config/mongodb.config.ts
+++ src/shared/config/mongodb.config.ts
@@ -1,16 +1,15 @@
 import { registerAs } from '@nestjs/config';
 
-const isDevelopment = process.env.NODE_ENV !== 'production';
+// const isDevelopment = process.env.NODE_ENV !== 'production';
 
 /**
  * Mongo database connection config
  */
 export default registerAs('mongodb', () => {
-    const { MONGO_DEV_URI, MONGO_PROD_URI } = process.env;
-    return {
-        uri: isDevelopment ? `${MONGO_DEV_URI}` : `${MONGO_PROD_URI}`,
-    };
+  const {
+    MONGO_URI
+  } = process.env;
+  return {
+    uri:`${MONGO_URI}`,
+  };
 });
-
-
-
